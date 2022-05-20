7dc6ee48b21566cda706c02478483f20022851d9
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2022-05-20 15:58:29 +0200

    clean data and some edit

--- SVMmodel.py
+++ SVMmodel.py
@@ -5,13 +5,18 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 import pickle
-import joblib
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++                    Coded BY : Ahmed Hassan                                ++++++ 
+#++++++                             : Abdelrhman Yasser                           ++++++ 
+#++++++                             : Mahmoud Rabea                               ++++++ 
+#++++++                             : Misara Ahmed                                ++++++                                         
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #Read Data From CSV File 
 df = pd.read_csv("diabetes.csv")
 #Data cleaning from nonlogic values
 for i in range(len(df['Insulin'])):
   if df['Insulin'][i] == 0:
-    df['Insulin'][i] = df['Insulin'].mean()
+        df['Insulin'][i] = df['Insulin'].mean()
   if df['BloodPressure'][i]<60:
     df['BloodPressure'][i] = df['BloodPressure'].mean()
   if df['SkinThickness'][i] < 3.3:
@@ -19,9 +24,11 @@ for i in range(len(df['Insulin'])):
 #Set X as features of our model and Y as the prediction of the model
 X = df.drop(columns = 'Outcome', axis=1)
 Y = df['Outcome']
+#X_test = dg.drop(columns = 'Outcome', axis=1)
+#Y_test = dg['Outcome']
+#Split data to train set and test set
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
-print(X.shape, X_train.shape, X_test.shape)
-
+#print(X.shape, X_train.shape, X_test.shape)
 classifier = svm.SVC(kernel='linear')
 
 #training the support vector Machine Classifier
@@ -42,6 +49,7 @@ print('Accuracy score of the test data : ', test_data_accuracy)
 input_data =[]
 data_type =[
 "Pregnancies",	"Glucose","BloodPressure","SkinThickness","Insulin","BMI","DiabetesPedigreeFunction","Age"]
+#Take data from user if you want to predict
 for i in range(0,8):
   x=float(input("Enter Paitent"+data_type[i]+":"))
   input_data.append(x)
@@ -52,7 +60,8 @@ input_data_as_numpy_array = np.asarray(input_data)
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 prediction = classifier.predict(input_data_reshaped)
 print(prediction)
-pickle.dump(classifier,open('final_model.sav','wb'))
+#Saving the model to sav file
+#pickle.dump(classifier,open('final_model.sav','wb'))
 if (prediction[0] == 0):
   print('The person is not diabetic')
 else:
